db3961962223857a625177ee79fd5e5515e3fb21
hashtag-squirrel <alita2000@web.de>
2025-04-18 15:59:42 +0200

    Move react app into site/frontend directory and update dockerfile

--- site/frontend/src/App.js
+++ site/frontend/src/App.js
@@ -1,22 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Builder from './pages/Builder';
+import Preview from './pages/Preview';
+import Dashboard from './pages/Dashboard';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import { PortfolioProvider } from './context/PortfolioContext';
+import GithubCallback from './components/github/GithubCallback';
+import './styles/global.css';
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
-  const [currentDate, setCurrentDate] = useState(0);
-  useEffect(() => {
-  fetch(' http://127.0.0.1:8000/').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-      setCurrentDate(data.date)
-    });
-  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-      <p>The date is {currentDate} and the time is {currentTime}.</p> <br/>
-
-      </header>
-    </div>
+    <PortfolioProvider>
+      <Router>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/builder" element={<Builder />} />
+            <Route path="/preview" element={<Preview />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/github/callback" element={<GithubCallback />} />
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
+    </PortfolioProvider>
   );
 }
 
