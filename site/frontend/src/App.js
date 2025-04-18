01c60a8e797b8051d2f1921939b7f02ae82d131d
Goncalves95 <138715742+Goncalves95@users.noreply.github.com>
2025-04-18 17:03:29 +0200

    creat a mock for user in development stage

--- site/frontend/src/App.js
+++ site/frontend/src/App.js
@@ -6,13 +6,13 @@ import { PortfolioProvider } from './context/PortfolioContext';
 // Componentes
 import Navbar from './components/layout/Navbar';
 import Home from './pages/Home';
-import Login from '../../site/frontend/src/components/login-sign/Login';
-import Register from '../../site/frontend/src/components/login-sign/Register';
-import Dashboard from '../../site/frontend/src/pages/Dashboard';
+import Login from '../src/components/login-sign/Login';
+import Register from '../src/components/login-sign/Register';
+import Dashboard from '../src/pages/Dashboard';
 import Builder from './pages/Builder';
 import Preview from './pages/Preview';
 import GithubCallback from './components/github/GithubCallback';
-import PrivateRoute from '../../site/frontend/src/components/common/PrivateRoute';
+import PrivateRoute from '../src/components/common/PrivateRoute';
 
 function App() {
   return (
@@ -25,8 +25,6 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/github/callback" element={<GithubCallback />} />
-            
-            {/* Rotas protegidas */}
             <Route element={<PrivateRoute />}>
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/builder" element={<Builder />} />
