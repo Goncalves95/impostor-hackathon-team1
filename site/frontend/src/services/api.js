3ea2a11a52d09683c6c475b6a03854f9b1e2d823
Goncalves95 <138715742+Goncalves95@users.noreply.github.com>
2025-04-20 17:01:34 +0200

    add backend url

--- site/frontend/src/services/api.js
+++ site/frontend/src/services/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-const API_BASE_URL = 'https://p01--backend--nq9vw2fny6kl.code.run/';
-
+const API_BASE_URL = ' https://leveluphub-backend.onrender.com';
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
