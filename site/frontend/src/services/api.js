c28a6adb42d8d283ea209eac76315f302abf0fd9
Goncalves95 <138715742+Goncalves95@users.noreply.github.com>
2025-04-21 10:38:47 +0200

    correting path to backend

--- site/frontend/src/services/api.js
+++ site/frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_BASE_URL = ' https://leveluphub-backend.onrender.com';
+const API_BASE_URL = 'https://leveluphub-backend.onrender.com';
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
